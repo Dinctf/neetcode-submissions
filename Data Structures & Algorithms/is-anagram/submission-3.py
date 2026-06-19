4c54d3caa971eaa6a2d25791d372c274b8030953
Dinctf <dineshofficial312@gmail.com>
2026-06-19 18:47:16 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        k={}
+        h={}
+        for char in s:
+            if s in k:
+                k[char]+=1
+            else:
+                k[char]=1
+        for char in t:
+            if t in h:
+                h[char]+=1
+            else:
+                h[char]=1
+        if k==h:
+            return True
+        else:
+            return False
+        
+        
+        
